f9a3db0e796e27ff0f110f9787e877ac2c353ca7
IvanHrozny2 <96377288+IvanHrozny2@users.noreply.github.com>
2023-05-03 14:30:28 +0300

    Change MyScrollingScreen

---
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
@@ -0,0 +1,40 @@
+package com.raywenderlich.android.lab1.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.raywenderlich.android.lab1.router.BackButtonHandler
+import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import com.raywenderlich.android.lab1.router.Screen
+
+@Composable
+fun TextFieldScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        MyTextField()
+    }
+
+    BackButtonHandler {
+        FundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun MyTextField() {
+    val textValue = remember {mutableStateOf("")}
+
+    TextField(
+        value = textValue.value,
+        onValueChange = {textValue.value = it},
+        label ={}
+    )
+}
